b5441355107e155146c671378c876245af20f799
Paolo Pochetti <p4olo-90@mac-pro-di-paolo-pochetti.local>
2013-09-08 10:37:06 +0200

    Initial Commit

---
+++ WhoisThis?/RL8X69KVKTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RL8X69KVKTAppDelegate.h
+//  WhoisThis?
+//
+//  Created by Paolo Pochetti on 08/09/13.
+//  Copyright (c) 2013 iAppear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RL8X69KVKTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WhoisThis?/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WhoisThis?
+//
+//  Created by Paolo Pochetti on 08/09/13.
+//  Copyright (c) 2013 iAppear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RL8X69KVKTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RL8X69KVKTAppDelegate class]));
+    }
+}
